7aa17af5df8f49d8f1fd6e7e7164b9a83de6def8
shrishabh <rkhandelwal3@wisc.edu>
2018-05-16 13:40:06 -0500

    adding the code to read the root file

--- skeletonCode.cc
+++ skeletonCode.cc
@@ -112,10 +112,27 @@ int main(int argc, char **argv) {    // this is for manual power threshold value
   //cout << "GetGain(700,5,0,0) is " << detector->GetGain(700., 5., 0., 0) << "\n";
   //cout << "GetGain(10,5,0,0) is " << detector->GetGain(10., 5., 0., 0) << "\n";
 
-  cout<<"station x is "<<detector->stations[0].GetX()<<endl;
-  cout<<"string x is "<<detector->stations[0].strings[0].GetX()<<endl;
-  cout<<"antenna x is "<<detector->stations[0].strings[0].antennas[0].GetX()<<endl;
+  // cout<<"station x is "<<detector->stations[0].GetX()<<endl;
+  // cout<<"string x is "<<detector->stations[0].strings[0].GetX()<<endl;
+  // cout<<"antenna x is "<<detector->stations[0].strings[0].antennas[0].GetX()<<endl;
   //cout<<"antenna Gain(700,5,0) is "<<detector->stations[0].strings[0].antennas[0].GetG(detector,700.,5.,0.)<<endl;
+
+  //Get the station coordinates and string coordinates printed out.
+
+/* This should be a part of the skeleton code, in case you want to look back
+
+  cout<<"station x is "<<detector->stations[0].GetX()<<endl;
+  cout<<"station y is "<<detector->stations[0].GetY()<<endl;
+  cout<<"station z is "<<detector->stations[0].GetZ()<<endl;
+  double depth = detector->stations[0].GetZ();
+  for (int i = 0; i < detector->params.number_of_strings;i++){
+    cout<<" String "<<i<<" X = "<<(double)detector->stations[0].strings[i].GetX()<<endl;
+    cout<<" String "<<i<<" Y = "<<(double)detector->stations[0].strings[i].GetY()<<endl;
+    for (int j = 0; j < detector->params.number_of_antennas_string;j++){
+      cout<<" String "<<i<<" Antenna "<<j<<" Z = "<<(double)detector->stations[0].strings[i].antennas[j].GetZ() - depth<<endl;
+    }
+  }
+*/
 
   cout<<"params.number_of_stations : "<<detector->params.number_of_stations<<endl;
   cout<<"params.station_spacing : "<<detector->params.station_spacing<<endl;
@@ -142,10 +159,38 @@ int main(int argc, char **argv) {    // this is for manual power threshold value
   AraTree2->GetEvent(1);
   cout<<"nnu x : "<<event->Nu_Interaction[0].nnu.GetX()<<endl;
 
+  cout<<"Vmmhz size "<<report->stations[0].strings[0].antennas[0].vmmhz.size()<<endl;
+  cout<<"Vmmhz value "<<report->stations[0].strings[0].antennas[0].vmmhz[1].size()<<endl;
+
+
+
   int nnu_pass = 0; // number of nu events which passed PickUnbiased.
-  double posnuX[settings->NNU];
-  double posnuY[settings->NNU];
-  double posnuR[settings->NNU];
+  double posnuX;
+  double posnuY;
+  double posnuZ;
+  double posnuR;
+  double posnuTheta;
+  double posnuPhi;
+
+  double dirnuX;
+  double dirnuY;
+  double dirnuZ;
+  double dirnuR;
+  double dirnuTheta;
+  double dirnuPhi;
+
+  string current;
+
+  vector <double> vmmhz1m;
+  vector <int> signalBin;
+
+  double emfrac;
+  double hadfrac;
+
+
+
+
+
   double posnuX_max=-icemodel->R_EARTH;
   double posnuY_max=-icemodel->R_EARTH;
 
@@ -153,13 +198,17 @@ int main(int argc, char **argv) {    // this is for manual power threshold value
   double posnuY_min=icemodel->R_EARTH;
 
 
-
+  ofstream outputFile;
+  char nameofOutputFile[200];
+  sprintf(nameofOutputFile,"eventInfo.txt");
+  outputFile.open(nameofOutputFile);
 
   ///////////////////////////////////////////
   //  test Detector class
   ///////////////////////////////////////////
 
-
+  int bin = settings->NFOUR/2;
+  double wf_time_offset = -450.;// in ns
 
 
   if ( settings->DETECTOR != 0 ) {
@@ -168,19 +217,60 @@ int main(int argc, char **argv) {    // this is for manual power threshold value
       for (int inu=0;inu<settings->NNU;inu++) { // loop over neutrinos
 
 
-          AraTree2->GetEvent(inu);
+          AraTree2->GetEvent(inu); // this is for report and event class
+          AraTree->GetEvent(inu); // this is for trigger, detector and other classes
 
           // save X, Y of posnus which passed PickUnbiased
-          if ( event->Nu_Interaction[0].pickposnu ) {
-              posnuX[nnu_pass] = event->Nu_Interaction[0].posnu.GetX();
-              posnuY[nnu_pass] = event->Nu_Interaction[0].posnu.GetY();
-              posnuR[nnu_pass] = event->Nu_Interaction[0].posnu.R();
+          if ( report->stations[0].Global_Pass > 0) {
+              //The position of all the events
+              posnuX = event->Nu_Interaction[0].posnu.GetX();
+              posnuY = event->Nu_Interaction[0].posnu.GetY();
+              posnuZ = event->Nu_Interaction[0].posnu.GetZ();
+              posnuR = event->Nu_Interaction[0].posnu.R();
+              posnuTheta = event->Nu_Interaction[0].posnu.Theta();
+              posnuPhi = event->Nu_Interaction[0].posnu.Phi();
 
-              if (posnuX[nnu_pass] > posnuX_max) posnuX_max = posnuX[nnu_pass];
-              if (posnuX[nnu_pass] < posnuX_min) posnuX_min = posnuX[nnu_pass];
+              // if (posnuX[nnu_pass] > posnuX_max) posnuX_max = posnuX[nnu_pass];
+              // if (posnuX[nnu_pass] < posnuX_min) posnuX_min = posnuX[nnu_pass];
+              //
+              // if (posnuY[nnu_pass] > posnuY_max) posnuY_max = posnuY[nnu_pass];
+              // if (posnuY[nnu_pass] < posnuY_min) posnuY_min = posnuY[nnu_pass];
 
-              if (posnuY[nnu_pass] > posnuY_max) posnuY_max = posnuY[nnu_pass];
-              if (posnuY[nnu_pass] < posnuY_min) posnuY_min = posnuY[nnu_pass];
+              dirnuX = event->Nu_Interaction[0].nnu.GetX();
+              dirnuY = event->Nu_Interaction[0].nnu.GetY();
+              dirnuZ = event->Nu_Interaction[0].nnu.GetZ();
+              dirnuR = event->Nu_Interaction[0].nnu.R();
+              dirnuTheta = event->Nu_Interaction[0].nnu.Theta();
+              dirnuPhi = event->Nu_Interaction[0].nnu.Phi();
+
+              emfrac = event->Nu_Interaction[0].emfrac;
+              hadfrac = event->Nu_Interaction[0].hadfrac;
+
+              //signalBin = report->signal_bin;
+              current = event->Nu_Interaction[0].current;
+              outputFile<<"Event Number "<<nnu_pass<<endl;
+
+              outputFile<<"Position (Cartesian) (X,Y,Z) :"<<posnuX<<", "<<posnuY<<", "<<posnuZ<<endl;
+              outputFile<<"Position (Spherical) (R, Theta, Phi) :"<<posnuR<<" "<<posnuTheta<<" "<<posnuPhi<<endl;
+              outputFile<<"Direction (Cartesian)  :"<<dirnuX<<", "<<dirnuY<<", "<<dirnuZ<<endl;
+              outputFile<<"Position (Spherical)  :"<<dirnuR<<" "<<dirnuTheta<<" "<<dirnuPhi<<endl;
+              outputFile<<"Em frac : "<<emfrac<<" Hadronic frac : "<<hadfrac<<endl;
+              outputFile<<"Type of Interaction "<<current;
+              outputFile<<endl;
+              outputFile<<" Waveforms - without Diode Convolution"<<endl;
+              for (int i = 0; i < 4; i++){
+                for (int j = 0; j < 4; j++){
+                  for (int l=0; l<bin; l++) {
+                    outputFile<<report->stations[0].strings[i].antennas[j].V_mimic[l]<<" ";
+                  }
+                }
+              }
+              outputFile<<endl;
+
+
+
+              outputFile<<endl;
+              outputFile<<endl;
 
               nnu_pass++;
 
